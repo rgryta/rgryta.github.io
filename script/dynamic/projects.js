40e9735b59b3490df6670a0108f8dd0b145faa98
rgryta <radek.gryta@gmail.com>
2022-06-22 17:19:24 +0200

    GH Pages - neverending story

--- script/dynamic/projects.js
+++ script/dynamic/projects.js
@@ -1,19 +1,80 @@
-function pageLoaded() {
-	console.log("Loading")
-	let projects = document.querySelectorAll("span.project")
-	projects.forEach(
-	  function(currentValue, currentIndex, listObj) {
-		let url = getMainAddress() + "content/project/" + listObj[currentIndex].getAttribute("name")
+var test = null
+var project = null
+var pname = null
+
+function setupProjectInfo(currentValue, currentIndex) {
+		pname = currentValue.getAttribute("name")
+		let url = getMainAddress() + "/project/" + pname
+	
 		
 		let xhr = new XMLHttpRequest()
 		xhr.open("GET", url)
+		xhr.responseType = 'document'
 
 		xhr.onload = function() {
 			if (this.readyState === this.DONE && this.status === 200) {
-				listObj[currentIndex].innerHTML = xhr.responseText
+				project = document.querySelector("div.project[name='"+pname+"']")
+				let doc = this.responseXML
+				test = this.responseXML
+				
+				let el = document.createElement("h2")
+				el.innerText = currentIndex+1
+				project.appendChild(el)
+				
+				el = document.createElement("h3")
+				el.innerText = doc.querySelector("#ptitle").innerText
+				project.appendChild(el)
+				
+				el = document.createElement("div")
+				el.setAttribute("style","font-size:smaller;")
+				el.innerText = doc.querySelector("#pshortdesc").innerText
+				project.appendChild(el)
+				
+				el = document.createElement("div")
+				el.classList.add("techstack")
+				project.appendChild(el)
+				
+				
+				let el_techstack = document.createElement("strong")
+				el_techstack.innerText = "Tech:"
+				el.appendChild(el_techstack)
+				
+				let techs = Array.from(doc.querySelectorAll("[tag='techstack']")).map(x => x.innerText)
+				techs.forEach(
+					function(currentValue, currentIndex, listObj) {
+						let el_techstack = document.createElement("span")
+						el_techstack.innerText = currentValue
+						el.appendChild(el_techstack)
+					}
+				)
+				
+				el = document.createElement("a")
+				el.setAttribute("href","/project/"+pname)
+				el.innerText = "Learn more"
+				project.appendChild(el)
+				
+				
 			}
 		}
 		xhr.send()
-	  }
-	);
+	}
+
+
+function pageLoaded() {
+	console.log("Loading")
+	
+	let sections = document.querySelectorAll("div.section")
+	
+	sections.forEach(
+		function(currentValue, currentIndex, listObj) {
+			let section = currentValue
+			let projects = section.querySelectorAll("div.project")
+			projects.forEach(
+				function(currentValue, currentIndex, listObj) {
+					setupProjectInfo(currentValue, currentIndex)
+				}
+			)
+		}
+	)
+	
 }
--- script/main.js
+++ script/main.js
@@ -1,21 +1,20 @@
 function getMainAddress() {
-    let subaddress = window.location.pathname.split('/')
-    subaddress = subaddress[subaddress.length - 1]
+    let subaddress = window.location.pathname
 
-    let url_length = window.location.href.length
+	let main_url = window.location.href.split('?')[0].split('#')[0]
+    let url_length = main_url.length
     let sub_lenght = subaddress.length
 
-    return window.location.href.substr(0, url_length - sub_lenght)
+    return main_url.substr(0, url_length - sub_lenght)
 }
 
 function getSubAddress(index = false) {
-    let subaddress = window.location.pathname.split('/')
-    subaddress = subaddress[subaddress.length - 1]
+    let subaddress = window.location.pathname
 
 	if (index) {
-		return subaddress == "" ? "index" : subaddress
+		return subaddress == "/" ? "/index" : subaddress
 	}
-    return subaddress == "index" ? "" : subaddress
+    return subaddress == "/index" ? "/" : subaddress
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -24,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     setNavBar()
     setFooter()
 	
-    setContent()
+    //setContent()
     setSiteMetaTags( getMainAddress() + getSubAddress() )
 	
 });
@@ -33,63 +32,93 @@ function setMeta() {
 
     let content = document.getElementsByTagName("head")[0]
 	
-    let url = getMainAddress() + "core/favicon/favicon.html"
-    setInner(url, content, true)
+    let url = getMainAddress() + "/core/favicon/favicon.html"
+    setInner(url, content, false, true)
 	
-    url = getMainAddress() + "core/meta.html"
-    setInner(url, content, true)
+    url = getMainAddress() + "/core/meta.html"
+    setInner(url, content, false, true)
 
 }
 
 function setContent() {
-    let url = getMainAddress() + "content/" + getSubAddress(true)
+    let url = getMainAddress() + getSubAddress(true)
 
-    let content = document.getElementsByClassName("content")[0]
+    let content = document.getElementById("content")
     setInner(url, content)
 }
 
 function setNavBarClicks(xhr, content) {
     if (xhr.readyState === XMLHttpRequest.DONE) {
+		content.querySelector("div[page='"+getSubAddress(true).substr(1)+"']").classList.add("current")
         for (i = 0; i < content.childElementCount; i++) {
-            let btn = content.children[i].children[0]
+            let btn = content.children[i]
             btn.addEventListener("click", function() {
                 let subaddress = this.getAttribute("page")
-                subaddress = subaddress == "index" ? "" : subaddress
+                subaddress = subaddress == "index" ? "/" : "/" + subaddress
                 let url = getMainAddress() + subaddress
-                history.pushState({}, null, url)
+				
+				if ( url != window.location.href.split('?')[0].split('#')[0] ) {
+					history.pushState({}, null, url)
+					
+					content.querySelector("div.current").classList.remove("current")
+					this.classList.add("current")
 
-                setContent()
-                setSiteMetaTags(url)
+					setContent()
+					setSiteMetaTags(url)
+				}
             });
         }
     }
 }
 
 function setNavBar() {
-    let url = getMainAddress() + "core/nav.html"
+    let url = getMainAddress() + "/core/nav.html"
 
-    let content = document.getElementsByClassName("nav")[0]
-    setInner(url, content, false, setNavBarClicks)
+    let content = document.getElementById("nav")
+    setInner(url, content, false, false, setNavBarClicks)
 }
 
 function setFooter() {
-    let url = getMainAddress() + "core/footer.html"
+    let url = getMainAddress() + "/core/footer.html"
 
-    let content = document.getElementsByClassName("footer")[0]
-    setInner(url, content)
+    let content = document.getElementById("footer")
+    setInner(url, content, false, false)
 }
 
-function setInner(url, invoker, append = false, finishFun = function() {}) {
+function setInner(url, invoker, doc = true, append = false, finishFun = function() {}) {
 
     let xhr = new XMLHttpRequest()
     xhr.open("GET", url)
 
-    xhr.onload = function() {
+	if (doc) {
+		xhr.responseType = "document"
+	}
+	
+    xhr.onload = async function() {
         if (this.readyState === this.DONE && this.status === 200) {
             if (append) {
-                invoker.innerHTML += xhr.responseText
+				if (doc) {
+					console.log(this.responseXML)
+					invoker.appendChild(this.responseXML)
+				}
+				else {
+					invoker.innerHTML += this.responseText
+				}
             } else {
-                invoker.innerHTML = xhr.responseText
+				if (doc) {
+					let el = this.responseXML.getElementById("content").children[0]
+					el.style.top="100%"
+					invoker.appendChild(el)
+					invoker.children[0].style.top="-100%"
+					await new Promise(r => setTimeout(() => r(), 300));
+					console.log("removing...")
+					invoker.children[0].remove()
+					el.style.top=null
+					
+				}
+				else {
+					invoker.innerHTML = this.responseText
+				}
             }
         }
         finishFun(xhr, invoker)
