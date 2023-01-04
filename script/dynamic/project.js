3e0717af27a0b1b293de542313d3531b7d290748
Radosław Gryta <radek.gryta@gmail.com>
2023-01-04 21:13:33 +0100

    Update for 2023 - added 2 projects
    Added Verified Bird and dockerized Stable-Diffusion for WSL2

--- script/dynamic/project.js
+++ script/dynamic/project.js
@@ -20,21 +20,6 @@ async function openComponent(evt, component) {
     })
 }
 
-function tiltIcon(e) {
-    let el = document.getElementById("picon")
-    let x = e.offsetX
-    let y = e.offsetY
-    let fullX = el.clientWidth
-    let fullY = el.clientHeight
-
-    let horizontal = x / fullX
-    let vertical = y / fullY
-    let rotateX = (50 / 2 - horizontal * 50).toFixed(2)
-    let rotateY = (vertical * 50 - 50 / 2).toFixed(2)
-    el.style.setProperty("--transform", "perspective(" + fullX + "px) rotateX(" + rotateY + "deg) rotateY(" + rotateX + "deg) scale3d(1, 1, 1)")
-
-}
-
 function parsePx(px) {
     return parseInt(px.replace(/px/, ""))
 }
@@ -95,12 +80,6 @@ function setupGalleries() {
 }
 
 function pageLoaded() {
-    console.log("Adding picon listener...")
-    let el = document.getElementById("picon")
-    el.addEventListener("mousemove", function(e) {
-        tiltIcon(e)
-    }, false)
-
     console.log("Setting up galleries...")
     setupGalleries()
 }
